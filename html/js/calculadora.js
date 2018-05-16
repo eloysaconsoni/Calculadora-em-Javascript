284387a04206cedebfa2cb9b129dee7a292e6bec
eloysaconsoni <eloysaconsoni@gmail.com>
2018-05-16 13:25:02 -0300

    ADD js e alteraçoes

---
+++ html/js/calculadora.js
@@ -0,0 +1,41 @@
+
+var num, cont=0,oper;
+
+
+function numero(num){
+	
+	num = value;
+	document.calculadora_sistemas.num7.value = num;
+
+}
+function numero(){
+
+
+for(cont =0;cont<num[];cont ++){
+
+	if(document.calculadora_sistemas.visor.value == x){
+
+		oper = document.calculadora_sistemas.visor.value;
+	}
+	else if (document.calculadora_sistemas.visor.value == +){
+
+	}
+	else if (document.calculadora_sistemas.visor.value == -) {
+
+	}
+	else if(document.calculadora_sistemas.visor.value == ){
+
+
+	}
+	else{
+		num[cont] = document.calculadora_sistemas.visor.value;
+	}
+	
+
+}
+
+}
+
+
+
+
