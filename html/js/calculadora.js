cd7a854c681adac134e1a06ddff1142d5e175f31
jorge <jorgeaugusto_99@hotmail.com>
2018-05-17 00:14:43 -0300

    BUG FIX no js

--- html/js/calculadora.js
+++ html/js/calculadora.js
@@ -1,45 +1,15 @@
+function preencher(valor){
 
-var num, cont=0,oper,val1,val2, result;
-
-
-
-function numero(num){
-	num = document.getElementsByName(value); 
-	alert('chegou aqui');
-
-
-		while(cont == 1){
-			val2 = document.calculadora_sistemas.visor.value;
-			calcula();
-		}
-
-
-		val1 = document.calculadora_sistemas.visor.value;
-		
-}
-function operacao(){
-	oper = document.calculadora_sistemas.visor.value; 	
-	cont = 1;
+	document.getElementById('visor').value += valor;
 
 }
-function calcula(){
-	switch(oper){
 
-		case x: result = val1 * val2;
-				return result;
+function calcular(){
+	var resultado = 0;
 
-		case +: 
+	resultado = document.getElementById('visor').value
 
-		case /: 
+	document.getElementById('visor').value = '';
 
-			
-
-		case -:
-	}
+	document.getElementById('visor').value = eval(resultado);	
 }
-
-
-
-
-
-
